751510d5cf128995ecd0a2f75aa0f7d5411e640b
Rogerio de Paula Assis <rogerio@freshmob.com.au>
2016-09-09 22:03:20 +1000

    AFSessionOperation

---
+++ AFSessionOperation/AFSessionOperation.h
@@ -0,0 +1,21 @@
+//
+//  AFSessionOperation.h
+//  AFSessionOperation
+//
+//  Created by Rogerio de Paula Assis on 9/09/2016.
+//  Copyright © 2016 Freshmob Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFSessionOperation.
+FOUNDATION_EXPORT double AFSessionOperationVersionNumber;
+
+//! Project version string for AFSessionOperation.
+FOUNDATION_EXPORT const unsigned char AFSessionOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFSessionOperation/PublicHeader.h>
+
+#import <AFSessionOperation/AFHTTPSessionOperation.h>
+#import <AFSessionOperation/AFURLSessionOperation.h>
+
