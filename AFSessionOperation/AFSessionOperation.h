0ad09a5ea9ff7ec5e1ccdd3f7fc6f5398c53375b
Rogerio de Paula Assis <rogerio@freshmob.com.au>
2016-09-15 12:13:37 +1000

    Framework fix

--- AFSessionOperation/AFSessionOperation.h
+++ AFSessionOperation/AFSessionOperation.h
@@ -16,4 +16,6 @@ FOUNDATION_EXPORT const unsigned char AFSessionOperationVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AFSessionOperation/PublicHeader.h>
 
+#import <AFSessionOperation/AFHTTPSessionOperation.h>
+#import <AFSessionOperation/AFURLSessionOperation.h>
 
